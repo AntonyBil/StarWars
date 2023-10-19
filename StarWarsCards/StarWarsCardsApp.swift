a18e2b6dae337a6b8286b2c85f9647ed975afbe2
AntonyBil <beletskiy.ant@gmail.com>
2023-10-19 20:20:47 +0300

    Initial commit

---
+++ StarWarsCards/StarWarsCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarWarsCardsApp.swift
+//  StarWarsCards
+//
+//  Created by apple on 19.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct StarWarsCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
